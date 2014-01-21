2809e4e8e4e358c2b3225149008aafe4f58d9de8
Jason Young <jason@ytechie.com>
2014-01-21 11:06:00 -0600

    Added support for creating the NuGet package

---
+++ Create NuGet Package.bat
@@ -0,0 +1,4 @@
+cd FluentMessaging
+nuget pack FluentMessaging.csproj
+move FluentMessaging.*.nupkg ..
+pause
--- FluentMessaging/Properties/AssemblyInfo.cs
+++ FluentMessaging/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FluentMessaging")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Declarative syntax for piping data in Azure Service Bus Message queues and other technologies.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FluentMessaging")]
